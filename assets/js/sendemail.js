dea83dcd1e1edaa9be27dfbb9e7071868e8c9568
Shinners888 <sinead888888888@gmail.com>
2021-01-10 22:01:45 +0000

    added functionality to contact form

---
+++ assets/js/sendemail.js
@@ -0,0 +1,31 @@
+function sendMail(contactForm) {
+ 
+emailjs.send("service_um2b8f8", "rosie", {
+    "from_name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "project_request": contactForm.projectsummary.value
+})
+    .then(function(response) {
+       console.log('SUCCESS!', response);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+    return false;
+}
+
+/*function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}*/
